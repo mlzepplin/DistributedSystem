e983f2b3b00bcc11b8f062ea2d3b94a76aff4ffd
Krishna Chaitanya <krishnachaitanya265@gmail.com>
2018-09-16 23:27:48 -0400

    Revert "A1"

--- proj1.exs
+++
@@ -1,3 +0,0 @@
-n = String.to_integer(List.first(System.argv))
-k = String.to_integer(List.last(System.argv))
-Dos1.Boss.fireSupervisor(n,k)
--- test/dos1_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule Dos1Test do
-  use ExUnit.Case
-  doctest Dos1
-
-  test "greets the world" do
-    assert Dos1.hello() == :world
-  end
-end
