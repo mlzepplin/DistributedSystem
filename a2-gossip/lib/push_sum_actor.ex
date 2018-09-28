7691ee82cbab0d6998ef1cb6bf28d33f72f64977
Rishabh Gupta <rishabh.gupta.1606@gmail.com>
2018-09-27 22:59:15 -0400

    pushsum actor

--- a2-gossip/lib/push_sum_actor.ex
+++ a2-gossip/lib/push_sum_actor.ex
@@ -1,79 +1,88 @@
 defmodule PushSumActor do
-    use GenServer
+  use GenServer
+
+  # Client Side
+  def start_link(default) do
+    GenServer.start_link(__MODULE__, default)
+  end
+
+  def get_ratio(pid)do
+    count = GenServer.call(pid,:get_ratio)
+  end
+
+  def add_peer(pid, item) do
+    GenServer.cast(pid, {:push, item})
+  end
+
+  def pushSum(pid,swPair) do
+    GenServer.cast(pid, {:push_sum,swPair})
+  end
+
+  def inform_main_of_hibernation(pid) do
+    GenServer.cast(pid, :hibernate)
+  end
+
+  # Server Side (callbacks)
+  def init(default) do
+    {:ok, default}
+  end
+
+  #################### Handle_Calls #####################
   
-    # Client Side
-    def start_link(default) do
-      GenServer.start_link(__MODULE__, default)
-    end
+  # pop
+  # def handle_call(:pop, _from, {s,w,round,main_pid,start_time,[head | tail]}) do
+  #   {:reply, head, start_time,{count,tail}}
+  # end
+
+  # # get_count
+  # def handle_call(:get_ratio, _from, {s,w,round,main_pid,start_time,neighbors} ) do
+  #   {:reply,div(s/w)}
+  # end
+
+  #################### Handle_Casts #####################
   
-    def get_count(pid)do
-      count = GenServer.call(pid,:get_count)
-    end
-  
-    def add_peer(pid, item) do
-      GenServer.cast(pid, {:push, item})
-    end
-  
-    def pushSum(pid) do
-        GenServer.cast(pid, :pushsum)
-    end
-  
-    def inform_main_of_hibernation(pid) do
-      GenServer.cast(pid, :hibernate)
-    end
-  
-    # Server Side (callbacks)
-    def init(default) do
-      {:ok, default}
-    end
-  
-    #################### Handle_Calls #####################
-    
-    # pop
-    def handle_call(:pop, _from, {count,main_pid,start_time,[head | tail]}) do
-      {:reply, head, start_time,{count,tail}}
-    end
-  
-    # get_count
-    def handle_call(:get_count, _from, {count,main_pid,start_time,neighbors} ) do
-      {:reply,count, start_time,{count,neighbors}}
-    end
-  
-    #################### Handle_Casts #####################
-    
-    # Gossip
-    def handle_cast(:gossip, {count,main_pid,start_time,neighbors}) do
-      current = self()
-      updatedState = 
-      case count >= 10 do 
-        true -> 
-          inform_main_of_hibernation(main_pid)
-          timeToHibernation = System.monotonic_time(:millisecond) - start_time
-          IO.inspect "hibernation time : #{ timeToHibernation}"
-          {:noreply, {count,main_pid,start_time,neighbors}}
-          #TODO - decide, to kill the node or not
-        false ->
-          forwardTo = Enum.random(neighbors)
-          IO.inspect current
-          IO.inspect forwardTo
-          IO.inspect count + 1
-          GossipActor.gossip(forwardTo) 
-          {:noreply, {count+1,main_pid,start_time,neighbors} }
+  # Push Sum
+  def handle_cast({:push_sum,{recievedS,recievedW}}, {s,w,[head|rest],main_pid,start_time,neighbors}) do
+    self = self()
+    updatedS = div(recievedS + s,2);
+    updatedW = div(recievedW + w,2);
+    pair = {updatedS,updatedW}
+    currentRatio = div(updatedS,updatedW)
+    forwardTo = Enum.random(neighbors)
+    if Enum.count(prevThreeVals)<3 do
+      #compute ratio 
+      #append it to prevThreeVals tail
+      pushSum(forwardTo,pair)
+      pushSum(self,pair)
+      {:noreply,{updatedS,updatedW,[head|rest|currentRatio],_}}
+
+    else
+      #compute ratio
+      #append it to prevThreeVals tail
+      #check if the currentratio - first entry in prevThreeVals > 10^-10
+        #if no --> terminate actor
+        #else send relevent message to random neighbor and self
+      if (currentRatio - head) < :math.pow(10,-10) do
+        IO.inspect "limit reached"
+        inform_main_of_hibernation(main_pid)
+        timeToHibernation = System.monotonic_time(:millisecond) - start_time
+        IO.inspect "time to hibernation : #{ timeToHibernation}"
+        {:noreply,{_}}
+      else
+        pushSum(forwardTo,pair)
+        pushSum(self,pair)
+        #remove the head from state
+        {:noreply,{updatedS,updatedW,[rest|currentRatio],_}}
       end
       
     end
-  
-    # push
-    def handle_cast({:push, item}, {count,main_pid,start_time,neighbors}) do
-      {:noreply, {count,main_pid,start_time,[item | neighbors]} }
-    end
-  
-    # step
-    def handle_cast(:step, {count,main_pid,start_time,neighbors}) do 
-        if count >= 1 do
-        self = self()
-        PushSumActor.pushSum(self)
-        end
-    end
-
+    
   end
+
+  # push
+  def handle_cast({:push, item}, {count,main_pid,start_time,neighbors}) do
+    {:noreply, {count,main_pid,start_time,[item | neighbors]} }
+  end
+
+
+end
