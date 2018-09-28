6395b4a1af993c4e6f1a77c953e3df7e55e371df
Rishabh Gupta <rishabh.gupta.1606@gmail.com>
2018-09-27 23:44:17 -0400

    push_sum state and terminating logic update

--- a2-gossip/lib/push_sum_actor.ex
+++ a2-gossip/lib/push_sum_actor.ex
@@ -34,54 +34,53 @@ defmodule PushSumActor do
   #   {:reply, head, start_time,{count,tail}}
   # end
 
-  # # get_count
-  # def handle_call(:get_ratio, _from, {s,w,round,main_pid,start_time,neighbors} ) do
-  #   {:reply,div(s/w)}
-  # end
+  # get_count
+  def handle_call(:get_ratio, _from, {s,w,prev_ratio,count,main_pid,start_time,neighbors} ) do
+    {:reply,s/w}
+  end
 
   #################### Handle_Casts #####################
   
   # Push Sum
-  def handle_cast({:push_sum,{recievedS,recievedW}}, {s,w,[head|rest],main_pid,start_time,neighbors}) do
+  def handle_cast({:push_sum,{recievedS,recievedW}}, {s,w,hibernated,prev_ratio,count,main_pid,start_time,neighbors}) do
     self = self()
-    updatedS = div(recievedS + s,2);
-    updatedW = div(recievedW + w,2);
+    updatedS = (recievedS + s)/2;
+    updatedW = (recievedW + w)/2;
     pair = {updatedS,updatedW}
-    currentRatio = div(updatedS,updatedW)
+    currentRatio = updatedS/updatedW
     forwardTo = Enum.random(neighbors)
-    if Enum.count(prevThreeVals)<3 do
-      #compute ratio 
-      #append it to prevThreeVals tail
-      pushSum(forwardTo,pair)
-      pushSum(self,pair)
-      {:noreply,{updatedS,updatedW,[head|rest|currentRatio],_}}
-
-    else
-      #compute ratio
-      #append it to prevThreeVals tail
-      #check if the currentratio - first entry in prevThreeVals > 10^-10
-        #if no --> terminate actor
-        #else send relevent message to random neighbor and self
-      if (currentRatio - head) < :math.pow(10,-10) do
-        IO.inspect "limit reached"
-        inform_main_of_hibernation(main_pid)
-        timeToHibernation = System.monotonic_time(:millisecond) - start_time
-        IO.inspect "time to hibernation : #{ timeToHibernation}"
-        {:noreply,{_}}
-      else
+    if hibernated do
+      {:noreply, {updatedS,updatedW,hibernated,currentRatio,count,main_pid,start_time,neighbors}}
+    else 
+    
+      if (abs(currentRatio - prev_ratio) < :math.pow(10,-10)) do
+      
+        if count==2 do
+          IO.inspect "limit reached"
+          inform_main_of_hibernation(main_pid)
+          timeToHibernation = System.monotonic_time(:millisecond) - start_time
+          IO.inspect "time to hibernation : #{ timeToHibernation}"
+          {:noreply, {updatedS,updatedW,true,currentRatio,count+1,main_pid,start_time,neighbors}}
+        else
+          pushSum(forwardTo,pair)
+          pushSum(self,pair)
+          {:noreply, {updatedS,updatedW,hibernated,currentRatio,count+1,main_pid,start_time,neighbors}}
+        end
+      
+      else 
         pushSum(forwardTo,pair)
         pushSum(self,pair)
-        #remove the head from state
-        {:noreply,{updatedS,updatedW,[rest|currentRatio],_}}
+        {:noreply, {updatedS,updatedW,hibernated,currentRatio,0,main_pid,start_time,neighbors}}
       end
-      
+    
     end
+  
     
   end
 
   # push
-  def handle_cast({:push, item}, {count,main_pid,start_time,neighbors}) do
-    {:noreply, {count,main_pid,start_time,[item | neighbors]} }
+  def handle_cast({:push, item}, {s,w,prev_ratio,count,main_pid,start_time,neighbors}) do
+    {:noreply, {s,w,prev_ratio,count,main_pid,start_time,[item | neighbors]} }
   end
 
 
